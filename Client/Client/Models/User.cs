ba16e96c0813091d210d307535fec912a686d0ca
leldenor <norenberga19@outlook.lv>
2020-12-06 17:45:14 +0100

    register page

--- Client/Client/Models/User.cs
+++ Client/Client/Models/User.cs
@@ -6,10 +6,12 @@ namespace Client.Models
     {
         [JsonPropertyName("id")] public string Id { get; set; }
         [JsonPropertyName("userName")] public string UserName { get; set; }
-        [JsonPropertyName("domain")] public string Domain { get; set; }
+        [JsonPropertyName("domain")] public string Email { get; set; }
         [JsonPropertyName("city")] public string City { get; set; }
         [JsonPropertyName("role")] public string Role { get; set; }
-        [JsonPropertyName("securityLevel")] public int SecurityLevel { get; set; }
+        [JsonPropertyName("firstName")] public string FirstName { get; set; }
+        [JsonPropertyName("lastName")] public string LastName { get; set; }
+        [JsonPropertyName("phone")] public string Phone { get; set; }
         [JsonPropertyName("password")] public string Password { get; set; }
     }
 }
